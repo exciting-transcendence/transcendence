e987bc5ab55b8756103a3419e68a8c0581074b86
saoh <saoh@student.42seoul.kr>
2022-08-01 16:58:11 +0900

    [Feat] db add

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'database',
+    port: 5432,
+    username: 'transcendence',
+    password: 'transcendence',
+    database: 'transcendence',
+    entities: [
+      __dirname + '/**/*.entity.{js,ts}'
+    ],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
