426cf43f746fb255648e52d134aedc1d83d33e00
scarf <greenscarf005@gmail.com>
2022-08-06 12:24:22 +0900

    fix: 오타 수정

--- frontend/app/src/Login.tsx
+++ frontend/app/src/Login.tsx
@@ -7,7 +7,7 @@ import FriendView from './FriendView'
 import UserSet from './UserSet'
 import axios from 'axios'
 import { Profile } from './components/Profile'
-app/src/Login.tsximport { mockUser } from './mock/mockUser'
+import { mockUser } from './mock/mockUser'
 
 const Loged = () => {
   const [state, setState] = useState(false)
