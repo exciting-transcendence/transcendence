4bc4639577a92e8bb9f8696d19968a9ba26351bb
scarf <greenscarf005@gmail.com>
2022-08-06 12:24:06 +0900

    feat: 블록된 사용자, 친구 추가된 사용자 추가

--- frontend/app/src/mock/mockUser.ts
+++ frontend/app/src/mock/mockUser.ts
@@ -5,12 +5,28 @@ export const mockUser: User = {
   name: 'Example User',
   avatar: 'https://picsum.photos/200',
   status: 'ONLINE',
-  friends: [],
-  blocks: [],
+  friends: ['friendID'],
+  blocks: ['blockedID'],
   stat: {
     wins: 2,
     loses: 4,
     draws: 4,
     rating: 3,
   },
+}
+
+export const blockedUser: User = {
+  ...mockUser,
+  id: 'blockedID',
+  name: 'Blocked User',
+  friends: [],
+  blocks: [],
+}
+
+export const friendUser: User = {
+  ...mockUser,
+  id: 'friendID',
+  name: 'Friend User',
+  friends: [],
+  blocks: [],
 }
