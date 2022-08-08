d404c628b4ef7d4a290e79eb7d41dbaf50ea3880
anduuin <jcm2232@gmail.com>
2022-08-08 12:47:29 +0900

    feat: 2FA 구현 (진행중)

--- backend/app/src/auth/auth.controller.ts
+++ backend/app/src/auth/auth.controller.ts
@@ -1,15 +1,42 @@
-// import {
-//   Controller,
-//   Get,
-//   UseGuards,
-//   Query,
-//   Req,
-//   Redirect,
-// } from '@nestjs/common'
+import { Controller, Put, UseGuards, Req, Delete, Get } from '@nestjs/common'
 
-// import { AuthGuard } from '@nestjs/passport'
-// import { UserService } from 'user/user.service'
-// import { FtOauthService } from 'user/ftOauth.service'
+import { TwoFactorService } from './two-factor.service'
+import { UserService } from 'user/user.service'
+import { JwtFtGuard } from './ft/jwt-ft.strategy'
+import { JwtUserGuard } from './jwt.strategy'
 
-// @Controller('api/auth')
-// export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly twoFactorService: TwoFactorService,
+  ) {}
+
+  @Put('/2fa')
+  @UseGuards(JwtUserGuard)
+  async enable(@Req() req: any) {
+    const { uid } = req.user
+
+    return {
+      qr: await this.twoFactorService.enable(uid),
+    }
+  }
+
+  @Delete('/2fa')
+  @UseGuards(JwtUserGuard)
+  async disable(@Req() req: any) {
+    const { uid } = req.user
+
+    await this.twoFactorService.disable(uid)
+  }
+
+  @Get('/2fa')
+  @UseGuards(JwtFtGuard)
+  async verify(@Req() req: any) {
+    const { uid } = req.user
+
+    return {
+      verified: await this.twoFactorService.verify(uid, req.query.token),
+    }
+  }
+}
