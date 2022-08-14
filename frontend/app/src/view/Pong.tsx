5c0e8f399baa67ca141eb6ffdd1f80885f8a2fe0
anduuin <jcm2232@gmail.com>
2022-08-14 13:51:05 +0900

    feat: 화면 중앙 정렬

--- frontend/app/src/view/Pong.tsx
+++ frontend/app/src/view/Pong.tsx
@@ -44,10 +44,10 @@ const PongUser = (props: { uid: number }) => {
   const profile = useRecoilValue(withPongProfile(props.uid))
 
   return (
-    <Stack>
+    <Stack justifyContent="center" alignItems="center">
       <Typography>{profile.nickname}</Typography>
       <Avatar src={profile.avatar} />
-      <Typography>{profile.rating}</Typography>
+      <Typography>RATING: {profile.rating}</Typography>
     </Stack>
   )
 }
@@ -88,50 +88,34 @@ const remainTimeModalStyle = {
   justifyContent: 'center',
   flexDirection: 'column',
 }
-
 const PongGrid = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -60%);
   display: grid;
 
-  grid-template-columns: 100px 300px 50px 300px 100px;
-  grid-template-rows: 50px 50px 600px;
+  grid-template-columns: 400px 400px;
+  grid-template-rows: 100px 450px;
 
   align-items: center;
   justify-items: center;
 `
 
-const PongScore = styled.div`
-  grid-column: 2 / 5;
+const PongLeftProfile = styled.div`
+  grid-column: 1 / 2;
   grid-row: 1 / 2;
 `
 
-const PongLeftScore = styled.div`
-  grid-column: 2 / 3;
-  grid-row: 2 / 3;
-`
-
-const PongVs = styled.div`
-  grid-column: 3 / 4;
-  grid-row: 2 / 3;
-`
-const PongRightScore = styled.div`
-  grid-column: 4 / 5;
-  grid-row: 2 / 3;
-`
-
-const PongLeftProfile = styled.div`
-  grid-column: 1 / 2;
-  grid-row: 3 / 4;
-`
-
 const PongCanvas = styled.canvas`
-  grid-column: 2 / 5;
-  grid-row: </PongGrid></PongGrid> 3 / 4;
+  grid-column: 1 / 3;
+  grid-row: 2 / 3;
   border: 1px solid black;
 `
 
 const PongRightProfile = styled.div`
-  grid-column: 5 / 6;
-  grid-row: 3 / 4;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
 `
 
 const Pong = (props: PongProps) => {
@@ -152,15 +136,40 @@ const Pong = (props: PongProps) => {
       const ctx = (pongCanvas.current as HTMLCanvasElement).getContext(
         '2d',
       ) as CanvasRenderingContext2D
-      ctx.clearRect(
+      ctx.fillStyle = 'black'
+      ctx.fillRect(
         0,
         0,
         props.window.height * props.window.ratio,
         props.window.height,
       )
+      ctx.fillStyle = 'white'
       drawRect(ctx, props.window, props.leftPaddle)
       drawRect(ctx, props.window, props.rightPaddle)
       drawRect(ctx, props.window, props.ball)
+      const fontSize = props.window.height * 0.1
+      ctx.font = `${fontSize}px monospace`
+      ctx.textBaseline = 'top'
+      ctx.textAlign = 'center'
+      ctx.fillText(
+        props.leftScore.toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+        (props.window.height * props.window.ratio) / 2 - fontSize,
+        0,
+      )
+      ctx.fillText(
+        props.rightScore.toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+        (props.window.height * props.window.ratio) / 2 + fontSize,
+        0,
+      )
+      const rectWidth = props.window.height * 0.025
+      for (let i = 0; i < 20; i++) {
+        ctx.fillRect(
+          (props.window.height * props.window.ratio) / 2 - rectWidth / 2,
+          (props.window.height / 20) * i + rectWidth * 0.5,
+          rectWidth,
+          rectWidth,
+        )
+      }
     })
     return () => cancelAnimationFrame(req)
   }, [props])
@@ -168,40 +177,30 @@ const Pong = (props: PongProps) => {
   console.log(props)
 
   return (
-    <RecoilRoot>
-      <PongGrid>
-        <Modal open={remainTime > 0}>
-          <Box sx={remainTimeModalStyle}>
-            <Typography variant="h1">{remainTime}</Typography>
-          </Box>
-        </Modal>
-        <Modal open={!!props.winner}>
-          <Box sx={remainTimeModalStyle}>
-            <Typography variant="h2">Winner is</Typography>
-            {props.winner === 'left' ? (
-              <PongUser uid={props.leftUser} />
-            ) : (
-              <PongUser uid={props.rightUser} />
-            )}
-          </Box>
-        </Modal>
-        <PongScore>SCORE</PongScore>
-        <PongLeftScore>{props.leftScore}</PongLeftScore>
-        <PongVs>VS</PongVs>
-        <PongRightScore>{props.rightScore}</PongRightScore>
-        <PongLeftProfile>
-          <PongUser uid={props.leftUser} />
-        </PongLeftProfile>
-        <PongCanvas
-          width={props.window.ratio * props.window.height}
-          height={props.window.height}
-          ref={pongCanvas}
-        />
-        <PongRightProfile>
-          <PongUser uid={props.rightUser} />
-        </PongRightProfile>
-      </PongGrid>
-    </RecoilRoot>
+    <PongGrid>
+      <Modal open={remainTime > 0}>
+        <Box sx={remainTimeModalStyle}>
+          <Typography variant="h1">{remainTime}</Typography>
+        </Box>
+      </Modal>
+      <Modal open={!!props.winner}>
+        <Box sx={remainTimeModalStyle}>
+          <Typography variant="h2">Winner is</Typography>
+          {props.winner === 'left' ? (
+            <PongUser uid={props.leftUser} />
+          ) : (
+            <PongUser uid={props.rightUser} />
+          )}
+        </Box>
+      </Modal>
+      <PongLeftProfile>
+        <PongUser uid={props.leftUser} />
+      </PongLeftProfile>
+      <PongRightProfile>
+        <PongUser uid={props.rightUser} />
+      </PongRightProfile>
+      <PongCanvas width={800} height={450} ref={pongCanvas} />
+    </PongGrid>
   )
 }
 
