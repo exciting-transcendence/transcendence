b13a0f65fce4a342b81b3c85e791ae51268bf526
scarf <greenscarf005@gmail.com>
2022-08-14 19:46:35 +0900

    refactor: recoil 훅 단순화

--- frontend/app/src/state/user.ts
+++ frontend/app/src/state/user.ts
@@ -1,39 +1,48 @@
+import axios from 'axios'
+import { User } from 'data'
 import { selector, selectorFamily } from 'recoil'
 import { withAuthFetchOption } from './auth'
+
+export const withUserRequest = selectorFamily({
+  key: 'WithUserRequest',
+  get:
+    <T>(endpoint: string | number) =>
+    async ({ get }) => {
+      const option = get(withAuthFetchOption)
+
+      try {
+        const { data } = await axios.get<T>(`/api/user/${endpoint}`, option)
+        return data
+      } catch (e) {
+        throw new Error(
+          `failed to fetch user endpoint ${endpoint}: reason: ${e}`,
+        )
+      }
+    },
+})
 
 export const withUser = selectorFamily({
   key: 'WithUser',
   get:
     (uid: number) =>
-    async ({ get }) => {
-      const option = get(withAuthFetchOption)
-
-      const res = await fetch(`/api/user/${uid}`, {
-        ...option,
-        method: 'GET',
-      })
-
-      if (res.ok) {
-        return await res.json()
-      } else {
-        throw new Error('Failed to get user')
-      }
-    },
+    async ({ get }) =>
+      get(withUserRequest<User>(uid)),
 })
 
 export const withMe = selector({
   key: 'WithMe',
-  get: async ({ get }) => {
-    const option = get(withAuthFetchOption)
-    const res = await fetch(`/api/user/me`, {
-      ...option,
-      method: 'GET',
-    })
+  get: async ({ get }) => get(withUserRequest<User>('me')),
+})
 
-    if (res.ok) {
-      return await res.json()
-    } else {
-      throw new Error('Failed to get my data')
-    }
+export const withUsers = selector({
+  key: 'WithUsers',
+  get: async ({ get }) => get(withUserRequest<User[]>('')),
+})
+
+export const withOtherUsers = selector({
+  key: 'WithOtherUsers',
+  get: async ({ get }) => {
+    const me = get(withMe)
+    return get(withUsers).filter((u) => u.uid !== me.uid)
   },
 })
