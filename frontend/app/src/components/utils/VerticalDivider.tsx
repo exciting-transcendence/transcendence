df4cd0b0f9359d7c1b3a8b449c033e054c0b1a8a
scarf <greenscarf005@gmail.com>
2022-08-14 16:14:35 +0900

    test: VerticalDivider 스토리 추가

--- frontend/app/src/components/utils/VerticalDivider.tsx
+++ frontend/app/src/components/utils/VerticalDivider.tsx
@@ -1,5 +1,10 @@
-import { Divider } from '@mui/material'
+import { Divider, DividerProps } from '@mui/material'
 
-export const VerticalDivider = () => (
-  <Divider orientation="vertical" flexItem style={{ marginRight: '-1px' }} />
+export const VerticalDivider = (props?: DividerProps) => (
+  <Divider
+    orientation="vertical"
+    flexItem
+    style={{ marginRight: '-1px' }}
+    {...props}
+  />
 )
