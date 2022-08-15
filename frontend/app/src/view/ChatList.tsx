61fb45d5b3ebb1dc8bf6c845ece6a979ea76ed0c
peas856 <42163769+peas856@users.noreply.github.com>
2022-08-16 00:11:41 +0900

    Update frontend/app/src/view/ChatList.tsx
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- frontend/app/src/view/ChatList.tsx
+++ frontend/app/src/view/ChatList.tsx
@@ -1,8 +1,6 @@
 import { Socket } from 'dgram'
 import { useState, useCallback, useEffect, useRef, useContext } from 'react'
-import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import Stack from '@mui/material/Stack'
+import { Box, Paper, Stack } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 type Room = {
