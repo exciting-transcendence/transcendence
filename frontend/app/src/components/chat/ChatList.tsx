a26577732f0ed3f91a7920e8114218137d309c25
scarf <greenscarf005@gmail.com>
2022-08-19 11:20:18 +0900

    refactor: 필요없는 fragment 제거

--- frontend/app/src/components/chat/ChatList.tsx
+++ frontend/app/src/components/chat/ChatList.tsx
@@ -11,21 +11,19 @@ export const ChatList = ({ chats }: Props) => {
   const groupedChats = groupBySerial(chats, (chat) => chat.senderUid)
 
   return (
-    <>
-      <List>
-        {groupedChats.map((chats) => {
-          const first = chats[0]
-          const { createdAt, senderUid: uid } = first
-          const { data, isSuccess } = useUserQuery<User>(uid) // TODO: 외부에서 전달
-          return (
-            <ChatListItem
-              user={isSuccess ? data : undefined}
-              key={createdAt.toISOString() + first.msgContent}
-              messages={chats.map((chat) => chat.msgContent)}
-            />
-          )
-        })}
-      </List>
-    </>
+    <List>
+      {groupedChats.map((chats) => {
+        const first = chats[0]
+        const { createdAt, senderUid: uid } = first
+        const { data, isSuccess } = useUserQuery<User>(uid) // TODO: 외부에서 전달
+        return (
+          <ChatListItem
+            user={isSuccess ? data : undefined}
+            key={createdAt.toISOString() + first.msgContent}
+            messages={chats.map((chat) => chat.msgContent)}
+          />
+        )
+      })}
+    </List>
   )
 }
