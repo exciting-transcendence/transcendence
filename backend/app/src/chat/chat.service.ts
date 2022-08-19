f585a963f2cfdbd56ce235f6a454976811fbe90c
saoh <saoh@student.42seoul.kr>
2022-08-19 15:38:24 +0900

    docs: avatar 문서화

--- backend/app/src/chat/chat.service.ts
+++ backend/app/src/chat/chat.service.ts
@@ -3,7 +3,6 @@ import {
   NotFoundException,
   InternalServerErrorException,
   BadRequestException,
-  UnauthorizedException,
 } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Not, Repository } from 'typeorm'
