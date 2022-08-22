a9e7249f44c0f3e484919c28eae24fa98015fbe5
scarf <greenscarf005@gmail.com>
2022-08-22 13:02:15 +0900

    refactor: useUserQuery 계열 쿼리로 변경

--- frontend/app/src/hook/useUserQuery.ts
+++ frontend/app/src/hook/useUserQuery.ts
@@ -6,31 +6,6 @@ import { MINUTE } from 'utility/time'
 import { getDataFn, ApiOptions } from './useApiQuery'
 export type UserApiKey = ['user', ...(string | number)[]]
 
-export const useUserQuery = (uid: number) => {
-  const { headers } = getAuthHeader()
-
-  return useQuery(['user', uid], async (): Promise<User> => {
-    let res = await fetch(`/api/user/${uid}`, { headers })
-
-    if (!res.ok) {
-      throw res
-    }
-
-    const data = await res.json()
-
-    res = await fetch(data.avatar, { headers })
-
-    if (!res.ok) {
-      throw res
-    }
-
-    const blob = await res.blob()
-    data.avatar = window.URL.createObjectURL(blob)
-
-    return data
-  })
-}
-
 const mapUserAvatar = async (user: User) => {
   const { headers } = getAuthHeader()
   const { data: blob } = await axios.get<Blob>(user.avatar, {
@@ -42,29 +17,37 @@ const mapUserAvatar = async (user: User) => {
   return user
 }
 
+// FIXME: 나중에 수정하기
+const mapChatUserAvatar = async (chatUser: ChatUser) => {
+  const { headers } = getAuthHeader()
+  const { data: blob } = await axios.get<Blob>(chatUser.user.avatar, {
+    headers,
+    responseType: 'blob',
+  })
+  chatUser.user.avatar = window.URL.createObjectURL(blob)
+  return chatUser
+}
+
 const getUserFn = (key: UserApiKey) => async () => {
   const user = await getDataFn<User>(key)()
   return mapUserAvatar(user)
 }
 const getUsersFn = (key: UserApiKey) => async () => {
   const users = await getDataFn<User[]>(key)()
-  return Promise.all(users.map(mapUserAvatar))
+  return Promise.all(users.map((user) => mapUserAvatar(user)))
 }
 const getChatUsersFn = (key: ['chat', number, 'list']) => async () => {
   const chatUsers = await getDataFn<ChatUser[]>(key)()
-  return Promise.all(
-    chatUsers.map((chatUser) => {
-      mapUserAvatar(chatUser.user as User)
-      return chatUser
-    }),
-  )
+  return Promise.all(chatUsers.map((chatUser) => mapChatUserAvatar(chatUser)))
 }
 
-export const useUserQuery2 = (key: UserApiKey) =>
+export const useUserQuery = (key: UserApiKey) =>
   useQuery<User, AxiosError>(key, getUserFn(key))
 
 export const useUsersQuery = (key: UserApiKey) =>
   useQuery<User[], AxiosError>(key, getUsersFn(key))
 
-export const useChatUsersQuery = (key: ['chat', number, 'list']) =>
-  useQuery<ChatUser[], AxiosError>(key, getChatUsersFn(key))
+export const useChatUsersQuery = (
+  key: ['chat', number, 'list'],
+  options?: ApiOptions,
+) => useQuery<ChatUser[], AxiosError>(key, getChatUsersFn(key), options)
