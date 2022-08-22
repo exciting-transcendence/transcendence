48863ac992fa02f04a297839ab21d5951474a374
scarf <greenscarf005@gmail.com>
2022-08-22 12:20:07 +0900

    feat: 아바타 수정해서 반환하는 useUserQuery 추가

--- frontend/app/src/hook/useUserQuery.ts
+++ frontend/app/src/hook/useUserQuery.ts
@@ -1,6 +1,10 @@
 import { getAuthHeader } from './getAuthHeader'
 import { useQuery } from '@tanstack/react-query'
-import { User } from 'data'
+import { ChatUser, OtherUser, User } from 'data'
+import axios, { AxiosError } from 'axios'
+import { MINUTE } from 'utility/time'
+import { getDataFn, ApiOptions } from './useApiQuery'
+export type UserApiKey = ['user', ...(string | number)[]]
 
 export const useUserQuery = (uid: number) => {
   const { headers } = getAuthHeader()
@@ -26,3 +30,41 @@ export const useUserQuery = (uid: number) => {
     return data
   })
 }
+
+const mapUserAvatar = async (user: User) => {
+  const { headers } = getAuthHeader()
+  const { data: blob } = await axios.get<Blob>(user.avatar, {
+    headers,
+    responseType: 'blob',
+  })
+
+  user.avatar = window.URL.createObjectURL(blob)
+  return user
+}
+
+const getUserFn = (key: UserApiKey) => async () => {
+  const user = await getDataFn<User>(key)()
+  return mapUserAvatar(user)
+}
+const getUsersFn = (key: UserApiKey) => async () => {
+  const users = await getDataFn<User[]>(key)()
+  return Promise.all(users.map(mapUserAvatar))
+}
+const getChatUsersFn = (key: ['chat', number, 'list']) => async () => {
+  const chatUsers = await getDataFn<ChatUser[]>(key)()
+  return Promise.all(
+    chatUsers.map((chatUser) => {
+      mapUserAvatar(chatUser.user as User)
+      return chatUser
+    }),
+  )
+}
+
+export const useUserQuery2 = (key: UserApiKey) =>
+  useQuery<User, AxiosError>(key, getUserFn(key))
+
+export const useUsersQuery = (key: UserApiKey) =>
+  useQuery<User[], AxiosError>(key, getUsersFn(key))
+
+export const useChatUsersQuery = (key: ['chat', number, 'list']) =>
+  useQuery<ChatUser[], AxiosError>(key, getChatUsersFn(key))
